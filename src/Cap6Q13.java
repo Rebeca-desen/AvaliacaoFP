e2c922e43b1bd703c85981dd3b5891dfbdc50c15
Rebeca-desen <rebecarss01@gmail.com>
2021-03-13 16:59:41 -0300

    tentativa de adicionar input para arrays

--- src/Cap6Q13.java
+++ src/Cap6Q13.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,18 @@
  * @author André
  */
 public class Cap6Q13 {
-    
+  public static void main(String [] args){
+    // Definir Array e limite de array para:
+    String alunos[][]=new String[8][2];
+    String VInformado;
+
+    // -nome alunos (8)
+    // -nota (8)    
+// Mensagem inicial dizendo o objetivo do programa
+    JOptionPane.showMessageDialog(null, "Esse é um programa que recebe o nome e nota dos alunos",null, JOptionPane.INFORMATION_MESSAGE);
+  
+    // Input para arrays
+    VInformado= JOptionPane.showInputDialog(null, "Adicione aque o nome do muleque", null, JOptionPane.QUESTION_MESSAGE);
+    // Retornar valores do input
+  }
 }
