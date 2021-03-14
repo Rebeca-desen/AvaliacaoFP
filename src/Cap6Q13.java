3cbf89b451d01499a6efb7fe18d997f5088bc3b3
Rebeca-desen <rebecarss01@gmail.com>
2021-03-14 17:02:49 -0300

    falhas resolvidas, rodando

--- src/Cap6Q13.java
+++ src/Cap6Q13.java
@@ -16,48 +16,41 @@ public class Cap6Q13 {
   public static void main(String [] args){
     // Definir Array e limite de array para:
     String alunos[]=new String[3];
-    float notas[]= new float[1];
-    String VInformado = null;
-   
-   
+    float notas[]= new float[3];
+    String nomeinf;
+    String notainf;
 
     // -nome alunos (8)
     // -nota (8)    
 // Mensagem inicial dizendo o objetivo do programa
-    JOptionPane.showMessageDialog(null, "Esse é um programa que recebe o nome e nota dos alunos",null, JOptionPane.INFORMATION_MESSAGE);
     
-  
-    // Input para arrays
-  for(int i=0; i <= alunos.length; i++){
-    VInformado= JOptionPane.showInputDialog(null, "Adicione aque o nome do muleque", null, JOptionPane.QUESTION_MESSAGE);
-    alunos[i]= VInformado;
-     for (int iNotas=0; iNotas < notas.length; iNotas++){
-        VInformado= JOptionPane.showInputDialog(null, "Adicione a nota dele", null, JOptionPane.QUESTION_MESSAGE);
-        notas[i]= Float.valueOf(VInformado);
-  
+       
+   
+        JOptionPane.showMessageDialog(null, "Esse é um programa que recebe o nome e nota dos alunos");
+   
+        for(int i=0; i < alunos.length; i++){
+            nomeinf= JOptionPane.showInputDialog(null, "Adicione aque o nome do muleque");
+            alunos[i]= nomeinf;
+            notainf= JOptionPane.showInputDialog(null, "Adicione a nota dele");
+            notas[i]= Float.valueOf(notainf);
+ 
      }
-      }
-  //Return das arrays
-  JOptionPane.showMessageDialog(null, "essa coisa sai do bagui", null, JOptionPane.INFORMATION_MESSAGE);
-     
-    for(int i=0; i < 3; i++){
-       JOptionPane.showMessageDialog(null, "essa misera entra aqui", null, JOptionPane.INFORMATION_MESSAGE);
-        alunos[i]= VInformado;
-     if(alunos[i]==null){
-      continue;
-      }else{
-      for(int iNotas=0; iNotas < 1; iNotas++){
-          notas[iNotas]= Float.valueOf(VInformado);
-          if(notas[iNotas]==0){
-              continue;
-          }else{
-                 JOptionPane.showMessageDialog(null, "Relatório final dos alunos e suas notas:" 
-                + (i+1) + alunos[i] + "-" + notas[i]);
-          }
-  }
-  }
-  
+        for(int i=0; i < alunos.length; i++){
+               
+                JOptionPane.showMessageDialog(null,"O Relatório Final dos alunos é:\n"+alunos[i]+" - "+notas[i]);
+       
     }
   }
 }
+
+  
+    // Input para arrays
+  
+  //Return das arrays
+  
+        
+  
+    
+  
+
 
