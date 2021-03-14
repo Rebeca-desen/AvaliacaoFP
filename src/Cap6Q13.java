54869154653dad3e614225c5f66a5a6c991b4c38
Rebeca-desen <rebecarss01@gmail.com>
2021-03-14 17:25:00 -0300

    ajustes

--- src/Cap6Q13.java
+++ src/Cap6Q13.java
@@ -15,8 +15,8 @@ import javax.swing.JOptionPane;
 public class Cap6Q13 {
   public static void main(String [] args){
     // Definir Array e limite de array para:
-    String alunos[]=new String[3];
-    float notas[]= new float[3];
+    String alunos[]=new String[8];
+    float notas[]= new float[8];
     String nomeinf;
     String notainf;
 
@@ -27,24 +27,24 @@ public class Cap6Q13 {
        
    
         JOptionPane.showMessageDialog(null, "Esse é um programa que recebe o nome e nota dos alunos");
-   
+       // Input para arrays
         for(int i=0; i < alunos.length; i++){
-            nomeinf= JOptionPane.showInputDialog(null, "Adicione aque o nome do muleque");
+            nomeinf= JOptionPane.showInputDialog(null, "Adicione o nome do aluno(a) "+ (i+1)+":");
             alunos[i]= nomeinf;
-            notainf= JOptionPane.showInputDialog(null, "Adicione a nota dele");
+            notainf= JOptionPane.showInputDialog(null, "Adicione a nota dele(a)");
             notas[i]= Float.valueOf(notainf);
  
      }
         for(int i=0; i < alunos.length; i++){
-               
-                JOptionPane.showMessageDialog(null,"O Relatório Final dos alunos é:\n"+alunos[i]+" - "+notas[i]);
+                JOptionPane.showMessageDialog(null,"O Relatório Final dos alunos é:\n"+ "Aluno(a) "+alunos[i]+" - "+notas[i]);
+             
        
     }
   }
 }
 
   
-    // Input para arrays
+
   
   //Return das arrays
   
