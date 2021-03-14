af346c75d77d87fc5b0bcffb59741c924164b913
Rebeca-desen <rebecarss01@gmail.com>
2021-03-13 21:36:37 -0300

    adicionando alunos e notas

--- src/Cap6Q13.java
+++ src/Cap6Q13.java
@@ -14,16 +14,27 @@ import javax.swing.JOptionPane;
 public class Cap6Q13 {
   public static void main(String [] args){
     // Definir Array e limite de array para:
-    String alunos[][]=new String[8][2];
+    String alunos[]=new String[8];
+    float notas[]= new float[8];
     String VInformado;
+   
 
     // -nome alunos (8)
     // -nota (8)    
 // Mensagem inicial dizendo o objetivo do programa
     JOptionPane.showMessageDialog(null, "Esse é um programa que recebe o nome e nota dos alunos",null, JOptionPane.INFORMATION_MESSAGE);
+    
   
     // Input para arrays
+  for(int i=0; i < alunos.length; i++){
     VInformado= JOptionPane.showInputDialog(null, "Adicione aque o nome do muleque", null, JOptionPane.QUESTION_MESSAGE);
+    alunos[i]= VInformado;
+   for (int iNotas=0; iNotas < notas.length; iNotas++){
+        VInformado= JOptionPane.showInputDialog(null, "Adicione a nota dele", null, JOptionPane.QUESTION_MESSAGE);
+        notas[iNotas]= Float.valueOf(VInformado);
+   }
+    
     // Retornar valores do input
+  }
   }
 }
