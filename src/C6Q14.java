09b837a36248e9f38108fd03ac41f056939b5302
Rebeca-desen <rebecarss01@gmail.com>
2021-03-14 18:47:00 -0300

    organizando comentários para listar o que será feito, definindo variaveis e colocando mensagem inicial

--- src/C6Q14.java
+++ src/C6Q14.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,32 @@
  * @author André
  */
 public class C6Q14 {
-    
+   public static void main(String[] args){
+    //Variaveis
+    String alunos[]= new String[6];
+    float nota1[]= new float[6];
+    float nota2[]= new float[6];
+    float media;
+    float aprovados;
+    float reprovados;
+    String input_alunosEturma;
+    String input_notas;
+    String nome_turma;
+    //Mensagem inicial
+    JOptionPane.showMessageDialog(null, "Olá! Esse programa irá receber o nome dos alunos e suas notas, retorar a porcentagem dos que passaram, dos que reprovaram e a média da turma", null, JOptionPane.INFORMATION_MESSAGE);
+    //input nome da turma
+    //input alunos(6)
+    //input nota 1
+    //input nota 2
+    //Retornar as 3 notas de cada aluno
+    //Calcular média de cada aluno
+    //Calcular média da turma
+    //Calcular se foi aprovado ou não
+    //Calcular % de quantos foram aprovados e quantos não
+    //Se uma das notas for menor que 6 recuperação 
+    //Se as duas estão menores que 6, reprovado
+    //Quantos reprovados
+    //Quantos passaram
+        }
 }
+               
