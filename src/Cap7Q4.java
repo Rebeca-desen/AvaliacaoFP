05df948953159068c13534aab4066b800eb8f8dd
Rebeca-desen <rebecarss01@gmail.com>
2021-03-16 10:43:13 -0300

    Primeira tentativa

--- src/Cap7Q4.java
+++ src/Cap7Q4.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +14,21 @@
 public class Cap7Q4 {
    public static void main(String[] args){
     
-    String alunos_notas[][]= new String[][];
+    float[][] notas_provas= new float[3][2];
+    String[] alunes= new String[3];
+    String notas;
+    String provas;
+    
+    for(int iNotas=0; iNotas < notas_provas.length;  iNotas++){
+      for(int iProvas=0; iProvas < notas_provas.length; iProvas++){
+      
+      notas= JOptionPane.showInputDialog(null, "Adicione a nota da " + (iProvas+1) + "° " + "prova", null, JOptionPane.QUESTION_MESSAGE);
+      notas_provas[iNotas][iProvas]= Float.valueOf(notas);
+      
+      JOptionPane.showMessageDialog(null, notas_provas[iNotas][iProvas], null, JOptionPane.INFORMATION_MESSAGE);
+      }
+    
+    }
    
    }
 }
